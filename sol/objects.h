922ceef8c7bff4a9519d7c6cf426dc57f7e1e2f6
Anemona Maria M <57135826+anemonamaria@users.noreply.github.com>
2021-10-31 23:41:38 +0200

    Add files via upload

---
+++ sol/objects.h
@@ -0,0 +1,21 @@
+#ifndef OBJECTS_H
+#define OBJECTS_H
+
+// structure for an object to be placed in the sack, with its weight and profit
+typedef struct _sack_object
+{
+	int weight;
+	int profit;
+} sack_object;
+
+
+// structure for an individual in the genetic algorithm; the chromosomes are an array corresponding to each sack
+// object, in order, where 1 means that the object is in the sack, 0 that it is not
+typedef struct _individual {
+	int fitness;
+	int *chromosomes;
+    int chromosome_length;
+	int index;
+} individual;
+
+#endif
